a004d46082bf337919c5e8f7613455f25e1f7a80
umed-sable <sable92umesh@gmail.com>
2022-02-21 19:12:08 +0530

    todays new commit

---
+++ src/routes/logger.js
@@ -0,0 +1,10 @@
+
+let url = 'https://www.google.com'
+
+function printMyMessage(message){
+    console.log(message)
+}
+
+module.exports.endpoint = url
+
+module.exports.printMessage = printMyMessage
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,12 @@
+
+let obj = require('./logger')
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    obj.printMessage('thorium')
+    console.log(obj.endpoint)
     res.send('My first ever api!')
 });
 
